60ef5ea3af317bfc19f0e0037afc22ca5827dfe5
Danill_Ryazantsev <ilonmack@mail.ru>
2026-04-09 01:07:51 +0400

    Last,?

--- backend/TourCatalogAPI/Program.cs
+++ backend/TourCatalogAPI/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using TourCatalogAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+// Настройка порта для приложения
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 
